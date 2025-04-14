faaa28f5b5c605f7dd38e27ebc92ad80555ad08a
Zrt98 <82198860+Zrt98@users.noreply.github.com>
2025-04-14 17:40:46 +0800

    Add files via upload

---
+++ src/aoareceiver2.hpp
@@ -0,0 +1,6 @@
+#ifndef AOARECEIVER
+#define AOARECEIVER
+
+void *REC_BLE_ADV(void *pars);
+
+#endif // AOARECEIVER
---
+++ src/blegenerator.hpp
@@ -0,0 +1,6 @@
+#ifndef BLEGENERATOR
+#define BLEGENERATOR
+
+std::vector<int8_t> generatesamples(int tx_chan, int counter, int padbytes, uint8_t new_packet[],int len_packet,int flag);
+
+#endif // BLEGENERATOR
